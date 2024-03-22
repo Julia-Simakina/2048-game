8d03b7f76173067f2dc896ea4aa8834aea1c619b
Julia Simakina <simakina.js@yandex.ru>
2024-03-22 18:51:20 +0300

    feat: basic game logic

--- index.js
+++ index.js
@@ -1,252 +1,235 @@
-const gameField = document.querySelector(".game-container__field");
+const BOARD_SIZE = 4;
+const COMMON_NEW_VALUE = 2;
+const RARE_NEW_VALUE = 4;
+const GAMEOVER_TEXT = '=('
 
-let gameMatrix = Array.from({ length: 4 }, () => new Array(4).fill(0));
+const AvailableKeysEnum = {
+  ArrowLeft: true,
+  ArrowRight: true,
+  ArrowUp: true,
+  ArrowDown: true,
+};
+
+const gameField = document.querySelector(".game-container__field");
+const score = document.getElementsByClassName(
+  "game-container__scores-counter"
+)[0];
+let gameMatrix = Array.from({ length: BOARD_SIZE }, () => new Array(BOARD_SIZE).fill(0));
+
+let counter = 0;
+score.textContent = counter;
 
 //Ячейки grid
-function initializeGameBoard() {
+const initializeGameBoard = () => {
   for (let row = 0; row < gameMatrix.length; row++) {
     for (let col = 0; col < gameMatrix[row].length; col++) {
-      const maxCeilCoordX = gameMatrix[row].length;
-      const maxCeilCoordY = gameMatrix[col].length;
-
-      // console.log("x >>", maxCeilCoordX);
-      // console.log("y >>", maxCeilCoordY);
       const ceil = document.createElement("div");
       ceil.classList.add("game-container__cell");
-      // ceil.textContent = gameMatrix[row][col];
       ceil.setAttribute("id", `ceil_${[row]}_${[col]}`);
       gameField.append(ceil);
     }
   }
-}
+};
 
-const getRandomItem = (matrix) => {
+const getRandomCellCoords = (matrix) => {
   const newArr = [];
   matrix.forEach((row, indexRow) => {
     row.forEach((col, indexCol) => {
       if (matrix[indexRow][indexCol] > 0) {
         return;
-      } else {
-        newArr.push({ row: indexRow, col: indexCol });
       }
+
+      newArr.push({ row: indexRow, col: indexCol });
     });
   });
+
   const randomItem = newArr[Math.floor(Math.random() * newArr.length)];
+
   return randomItem;
 };
 
-function addNumInRandomCell() {
-  const newCeil = getRandomItem(gameMatrix);
+const addNumInRandomCell = () => {
+  const newCeil = getRandomCellCoords(gameMatrix);
+  gameMatrix[newCeil.row][newCeil.col] = Math.random() > 0.9 ? 4 : 2;
+};
 
-  //  console.log(newCeil)
-  if (newCeil === undefined) {
-    return alert("Game over!");
-  }
-  gameMatrix[newCeil.row][newCeil.col] = 2;
-}
-
-console.log(gameMatrix);
+const getCellClassNames = (num) => {
+  return `game-container__cell_title game-container__cell_title_${num}`;
+};
 
 // Перерисовка поля при кажом изменении
 const rerender = () => {
   for (let row = 0; row < gameMatrix.length; row++) {
     for (let col = 0; col < gameMatrix[row].length; col++) {
-      let ceil = document.getElementById(`ceil_${[row]}_${[col]}`);
+      let ceil = document.getElementById(`ceil_${row}_${col}`);
+      let value = gameMatrix[row][col];
+      let classes = getCellClassNames(value);
 
-      switch (gameMatrix[row][col]) {
-        case 0:
-          ceil.removeAttribute("class");
-          ceil.classList.add("game-container__cell");
-          ceil.textContent = "";
-          break;
-        case 2:
-          ceil.classList.add("game-container__cell_title");
-          ceil.classList.add("game-container__cell_title_2");
-          ceil.textContent = "2";
-          break;
-        case 4:
-          ceil.classList.add("game-container__cell_title");
-          ceil.classList.add("game-container__cell_title_4");
-          ceil.textContent = "4";
-          break;
-        case 8:
-          ceil.classList.add("game-container__cell_title");
-          ceil.classList.add("game-container__cell_title_8");
-          ceil.textContent = "8";
-          break;
-        case 16:
-          ceil.classList.add("game-container__cell_title");
-          ceil.classList.add("game-container__cell_title_16");
-          ceil.textContent = "16";
-          break;
-        case 32:
-          ceil.classList.add("game-container__cell_title");
-          ceil.classList.add("game-container__cell_title_32");
-          ceil.textContent = "32";
-          break;
-        case 64:
-          ceil.classList.add("game-container__cell_title");
-          ceil.classList.add("game-container__cell_title_64");
-          ceil.textContent = "64";
-          break;
-        case 128:
-          ceil.classList.add("game-container__cell_title");
-          ceil.classList.add("game-container__cell_title_128");
-          ceil.textContent = "128";
-          break;
-        case 256:
-          ceil.classList.add("game-container__cell_title");
-          ceil.classList.add("game-container__cell_title_256");
-          ceil.textContent = "256";
-          break;
-        case 512:
-          ceil.classList.add("game-container__cell_title");
-          ceil.classList.add("game-container__cell_title_512");
-          ceil.textContent = "512";
-          break;
-      }
+      ceil.className = classes;
+      ceil.textContent = value || "";
     }
   }
 };
 
-function moveLeft(event) {
-  if (event.code !== "ArrowLeft") {
-    return;
-  }
-  let matrixClone = [...gameMatrix];
-  for (let row = 0; row < matrixClone.length; row++) {
-    matrixClone[row] = slideRow(matrixClone[row]);
-    matrixClone[row] = mergeRow(matrixClone[row]);
-    matrixClone[row] = slideRow(matrixClone[row]);
-  }
-
-  if ( isEqualArray2(gameMatrix, matrixClone)) {
-    return;
-  } else {
-    gameMatrix = matrixClone;
-    addNumInRandomCell();
-    rerender();
-  }
-}
-
-function moveRight(event) {
-  if (event.code !== "ArrowRight") {
-    return;
-  }
-  let matrixClone = [...gameMatrix];
-  // console.log(gameMatrixClone);
-
-  for (let row = 0; row < matrixClone.length; row++) {
-    matrixClone[row] = slideRow(matrixClone[row]).reverse();
-    matrixClone[row] = mergeRow(matrixClone[row]);
-    matrixClone[row] = slideRow(matrixClone[row]).reverse();
-  }
-
- 
- 
-  if ( isEqualArray2(gameMatrix, matrixClone)) {
-    return;
-  } else {
-    gameMatrix = matrixClone;
-    addNumInRandomCell();
-    rerender();
-  }
-}
-
-function moveUp(event) {
-  if (event.code !== "ArrowUp") {
-    return;
-  }
-
-  for (let col = 0; col < gameMatrix[0].length; col++) {
-    let column = [];
-    for (let row = 0; row < gameMatrix.length; row++) {
-      column.push(gameMatrix[row][col]);
-    }
-    column = slideRow(column);
-    column = mergeRow(column);
-    column = slideRow(column);
-    for (let row = 0; row < gameMatrix.length; row++) {
-      gameMatrix[row][col] = column[row];
-    }
-  }
-
-  addNumInRandomCell();
-  rerender();
-  return gameMatrix;
-}
-
-function moveDown(event) {
-  if (event.code !== "ArrowDown") {
-    return;
-  }
-  let matrixClone = [...gameMatrix];
-  console.log(gameMatrix)
-
-  for (let col = 0; col < matrixClone[0].length; col++) {
-    let column = [];
-    for (let row = 0; row < matrixClone.length; row++) {
-      column.push(matrixClone[row][col]);
-    }
-    column = slideRow(column).reverse();
-    column = mergeRow(column);
-    column = slideRow(column).reverse();
-    for (let row = 0; row < matrixClone.length; row++) {
-      matrixClone[row][col] = column[row];
-    }
-  }
-  // if ( isEqualArray2(gameMatrix, matrixClone)) {
-  //   return;
-  // } else {
-    gameMatrix = matrixClone;
-    addNumInRandomCell();
-    rerender();
-  //  }
-
-  // return gameMatrix;
-}
-
-function slideRow(row) {
-  const arr = row.filter((num) => num !== 0);
+const slideRow = (row) => {
+  const arr = row.filter((num) => num);
   const missing = row.length - arr.length;
   const zeros = Array(missing).fill(0);
+
   return arr.concat(zeros);
-}
+};
+
+const isGameValuesChanged = (originalMatrix, updatedMatrix) => {
+  return JSON.stringify(originalMatrix) !== JSON.stringify(updatedMatrix);
+};
+
+const finishTurn = (originalMatrix, updatedMatrix) => {
+  if (!isGameValuesChanged(originalMatrix, updatedMatrix)) {
+    return;
+  }
+
+  gameMatrix = updatedMatrix;
+  moveCell();
+
+  if (isGameOver(gameMatrix)) {
+    setTimeout(() => alert("Game over!"), 1000);
+  }
+};
+
+const getGameMAtrixClone = () => {
+  return JSON.parse(JSON.stringify(gameMatrix));
+};
+
+const increaseScore = (value) => {
+  counter += value;
+  score.textContent = counter;
+};
 
 function mergeRow(row) {
   for (let i = 0; i < row.length - 1; i++) {
     if (row[i] === row[i - 1]) {
       row[i - 1] *= 2;
+      increaseScore(row[i - 1]);
       row[i] = 0;
     } else if (row[i] === row[i + 1]) {
       row[i] *= 2;
+
+      increaseScore(row[i]);
       row[i + 1] = 0;
     }
   }
+
   return row;
 }
 
-//Сравнение двух массивов
-function isEqualArray1(a1, a2) {
-  return a1.length === a2.length && a1.every((v,i)=> v === a2[i]);
+function isGameOver(matrix) {
+  // Проверить возможность объединения двух соседних клеток по горизонтали и вертикали
+  for (let row = 0; row < matrix.length; row++) {
+    for (let col = 0; col < matrix[row].length; col++) {
+      const current = matrix[row][col];
+      if (!current) {
+        return false;
+      }
+
+      if (col < matrix[row].length - 1 && current === matrix[row][col + 1]) {
+        return false;
+      }
+
+      if (row < matrix.length - 1 && current === matrix[row + 1][col]) {
+        return false;
+      }
+    }
+  }
+
+  return true;
 }
-//Сравнение двух  массивов
-function isEqualArray2(a1, a2) {
-  return a1.length === a2.length && a1.every((v,i)=> isEqualArray1(v, a2[i]));
-}
 
-
-
-document.addEventListener("keydown", moveDown);
-document.addEventListener("keydown", moveUp);
-document.addEventListener("keydown", moveLeft);
-document.addEventListener("keydown", moveRight);
-
-const startGame = () => {
-  initializeGameBoard();
-  addNumInRandomCell();
+function moveCell() {
   addNumInRandomCell();
   rerender();
+}
+
+const shakeDat = (originalArr, shouldReverse = false) => {
+  let arr = [...originalArr];
+  arr = shouldReverse ? slideRow(arr).reverse() : slideRow(arr);
+  arr = mergeRow(arr);
+  arr = shouldReverse ? slideRow(arr).reverse() : slideRow(arr);
+
+  return arr;
+};
+
+function moveLeft(matrixClone) {
+  for (let row = 0; row < matrixClone.length; row++) {
+    matrixClone[row] = shakeDat(matrixClone[row]);
+  }
+}
+
+function moveRight(matrixClone) {
+  for (let row = 0; row < matrixClone.length; row++) {
+    matrixClone[row] = shakeDat(matrixClone[row], true);
+  }
+}
+
+function moveUp(matrixClone) {
+  for (let col = 0; col < matrixClone[0].length; col++) {
+    let column = [];
+    for (let row = 0; row < matrixClone.length; row++) {
+      column.push(matrixClone[row][col]);
+    }
+    column = shakeDat(column);
+    for (let row = 0; row < matrixClone.length; row++) {
+      matrixClone[row][col] = column[row];
+    }
+  }
+}
+
+function moveDown(matrixClone) {
+  for (let col = 0; col < matrixClone[0].length; col++) {
+    let column = [];
+    for (let row = 0; row < matrixClone.length; row++) {
+      column.push(matrixClone[row][col]);
+    }
+
+    column = shakeDat(column, true);
+
+    for (let row = 0; row < matrixClone.length; row++) {
+      matrixClone[row][col] = column[row];
+    }
+  }
+}
+
+const move = (event) => {
+  if (!AvailableKeysEnum[event.code]) {
+    return;
+  }
+
+  let matrixClone = getGameMAtrixClone();
+
+  switch (event.code) {
+    case AvailableKeysEnum.ArrowLeft:
+      moveLeft(matrixClone);
+      break;
+    case "ArrowRight":
+      moveRight(matrixClone);
+      break;
+    case "ArrowUp":
+      moveUp(matrixClone);
+      break;
+    case "ArrowDown":
+      moveDown(matrixClone);
+      break;
+  }
+
+  finishTurn(gameMatrix, matrixClone);
+};
+
+const startGame = () => {
+
+  initializeGameBoard();
+  addNumInRandomCell();
+  moveCell();
 };
 
 startGame();
+
+document.addEventListener("keydown", move);
